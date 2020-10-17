e9daca52175daa4dd7a3ca8a6199792ff28b99b0
nestor <nestor.monrot.90@gmail.com>
2020-10-17 18:09:01 -0500

    adding pagination

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -10,6 +10,7 @@ import ListingDetail from './containers/ListingDetail';
 import Login from './containers/Login';
 import SignUp from './containers/SignUp';
 import NotFound from './components/NotFound';
+import { HelmetProvider } from 'react-helmet-async';
 
 import { Provider } from 'react-redux';
 import store from './store';
@@ -17,22 +18,24 @@ import store from './store';
 import './sass/main.scss';
 
 const App = () => (
-  <Provider store={store}>
-    <Router>
-      <Layout>
-        <Switch>
-          <Route exact path='/' component={Home} />
-          <Route exact path='/about' component={About} />
-          <Route exact path='/contact' component={Contact} />
-          <Route exact path='/listings' component={Listings} />
-          <Route exact path='/listings/:id' component={ListingDetail} />
-          <Route exact path='/login' component={Login} />
-          <Route exact path='/signup' component={SignUp} />
-          <Route component={NotFound} />
-        </Switch>
-      </Layout>
-    </Router>
-  </Provider>
+  <HelmetProvider>
+    <Provider store={store}>
+      <Router>
+        <Layout>
+          <Switch>
+            <Route exact path='/' component={Home} />
+            <Route exact path='/about' component={About} />
+            <Route exact path='/contact' component={Contact} />
+            <Route exact path='/listings' component={Listings} />
+            <Route exact path='/listings/:id' component={ListingDetail} />
+            <Route exact path='/login' component={Login} />
+            <Route exact path='/signup' component={SignUp} />
+            <Route component={NotFound} />
+          </Switch>
+        </Layout>
+      </Router>
+    </Provider>
+  </HelmetProvider>
 );
 
 export default App;
