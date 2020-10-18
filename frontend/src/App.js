65a47a20c2f38946033430dbffe4f63e114f34cc
nestor <nestor.monrot.90@gmail.com>
2020-10-17 19:05:46 -0500

    adding contanct and about

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,6 +1,5 @@
 import React from 'react';
 import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
-import Layout from './hocs/Layout';
 
 import Home from './containers/Home';
 import About from './containers/About';
@@ -11,6 +10,9 @@ import Login from './containers/Login';
 import SignUp from './containers/SignUp';
 import NotFound from './components/NotFound';
 import { HelmetProvider } from 'react-helmet-async';
+
+import Layout from './hocs/Layout';
+import PrivateRoute from './components/privateRoute';
 
 import { Provider } from 'react-redux';
 import store from './store';
@@ -27,7 +29,7 @@ const App = () => (
             <Route exact path='/about' component={About} />
             <Route exact path='/contact' component={Contact} />
             <Route exact path='/listings' component={Listings} />
-            <Route exact path='/listings/:id' component={ListingDetail} />
+            <PrivateRoute exact path='/listings/:id' component={ListingDetail} />
             <Route exact path='/login' component={Login} />
             <Route exact path='/signup' component={SignUp} />
             <Route component={NotFound} />
--- frontend/src/containers/Home.js
+++ frontend/src/containers/Home.js
@@ -1,5 +1,5 @@
 import React, { useState } from 'react';
-import { Helmet } from 'react-helmet';
+import { Helmet } from 'react-helmet-async';
 import ListingForm from '../components/ListingForm';
 import Listings from '../components/Listings';
 import Pagination from '../components/Pagination';
